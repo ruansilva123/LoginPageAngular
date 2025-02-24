d44a9a83f0a091d07ca281b6ffbe9f0434761d2a
RuanCarlosSilva <143547755+ruansilva123@users.noreply.github.com>
2025-02-24 20:56:53 -0300

    :sparkles: feat: created sign up page

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -6,44 +6,48 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
-interface LoginForm {
+interface SignupForm {
+  name: FormControl,
   email: FormControl,
-  password: FormControl
+  password: FormControl,
+  confirmPassword: FormControl
 }
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-signup',
   imports: [
     DefaultLoginLayoutComponent,
     ReactiveFormsModule,
     PrimaryInputComponent
   ],
   providers: [LoginService],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
 })
-export class LoginComponent {
-  loginForm!: FormGroup<LoginForm>;
+export class SignupComponent {
+  signupForm!: FormGroup<SignupForm>;
 
   constructor(
     private router: Router,
     private loginService: LoginService,
     private toastr: ToastrService
   ){
-    this.loginForm = new FormGroup({
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8)])
     })
   }
 
   submit(){
-    this.loginService.login( this.loginForm.value.email, this.loginForm.value.password ).subscribe({
-      next: () => this.toastr.success("Login sucessfully!"),
-      error: () => this.toastr.error("Error logging in! Please try again later.")
+    this.loginService.login( this.signupForm.value.email, this.signupForm.value.password ).subscribe({
+      next: () => this.toastr.success("Sucessfully created!"),
+      error: () => this.toastr.error("Error create an accont! Please try again later.")
     })
   }
 
   navigate(){
-    this.router.navigate(["signup"]);
+    this.router.navigate(["login"]);
   }
 }
